e3600fd9e58fa1d2058ac9653991332228240a54
Kishore-Sharma <ks281222@gmail.com>
2023-03-19 06:15:16 -0400

    Fix

--- final_project/index.js
+++ final_project/index.js
@@ -12,9 +12,26 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+console.log("Inside Customer Authentication!");
+// Middleware which tells whether the customer is authenticated or not
+if(req.session.authorization) {
+    let token = req.session.authorization['accessToken']; // Access Token
+    jwt.verify(token, "access",(err,user)=>{
+        if(!err){
+            req.user = user;
+            next();
+        }
+        else{
+            return res.status(403).json({message: "User not authenticated"})
+        }
+     });
+ } else {
+     return res.status(403).json({message: "User not logged in"})
+ }
 });
  
 const PORT =5000;
+console.log("here");
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
